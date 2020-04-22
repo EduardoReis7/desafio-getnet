f5ea9bebd8498ad71a23eb944d7dc87f2f65569f
EduardoReis7 <eduardodovigireis@gmail.com>
2020-04-22 15:02:01 -0300

    Add classes Cliente

--- src/main/java/com/desafio/controllers/ClienteController.java
+++ src/main/java/com/desafio/controllers/ClienteController.java
@@ -1,9 +1,23 @@
 package com.desafio.controllers;
 
+import java.util.List;
+
+import javax.transaction.Transactional;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.desafio.repository.ClienteRepository;
+import com.desafio.controllers.form.AlterarNomeClienteForm;
+import com.desafio.dto.ClienteDto;
+import com.desafio.model.Cliente;
+import com.desafio.response.Response;
 import com.desafio.services.ClienteService;
 
 import lombok.RequiredArgsConstructor;
@@ -12,8 +26,51 @@ import lombok.RequiredArgsConstructor;
 @RequestMapping("/clientes")
 @RequiredArgsConstructor
 public class ClienteController {
-	
+
 	private final ClienteService clienteService;
-	
-	private final ClienteRepository clienteRepository;
+
+	@PostMapping
+	@Transactional
+	public ResponseEntity<?> cadastrar(@RequestBody ClienteDto dto) {
+
+		Response<ClienteDto> response = new Response<ClienteDto>();
+		ClienteDto clienteDto = clienteService.save(dto);
+		response.setData(clienteDto);
+
+		return ResponseEntity.status(HttpStatus.CREATED).body(response);
+	}
+
+	@GetMapping
+	public ResponseEntity<?> listar() {
+		List<Cliente> listaCliente = clienteService.listar();
+		return ResponseEntity.ok().body(listaCliente);
+	}
+
+	@GetMapping(value = "/{nomeCliente}")
+	public ResponseEntity<ClienteDto> buscarClientePeloNome(String nomeCliente) {
+		ClienteDto clienteDto = clienteService.buscarPorNomeCliente(nomeCliente);
+		return ResponseEntity.ok().body(clienteDto);
+	}
+
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<?> buscarClientePeloId(Long id) {
+		ClienteDto clienteDto = clienteService.buscarPorId(id);
+		return ResponseEntity.ok().body(clienteDto);
+	}
+
+	@PutMapping(value = "/{id}")
+	@Transactional
+	public ResponseEntity<?> alterarNomeCliente(Long id, @RequestBody AlterarNomeClienteForm form) {
+		Response<ClienteDto> response = new Response<ClienteDto>();
+		ClienteDto clienteDto = clienteService.alterarNomeCliente(id, form);
+		response.setData(clienteDto);
+		return ResponseEntity.status(HttpStatus.CREATED).body(response);
+	}
+
+	@DeleteMapping("/{id}")
+	@Transactional
+	public ResponseEntity<?> deletarClientePeloId(Long id) {
+		clienteService.removerPorId(id);
+		return ResponseEntity.noContent().build();
+	}
 }
--- src/main/java/com/desafio/model/Cliente.java
+++ src/main/java/com/desafio/model/Cliente.java
@@ -1,16 +1,35 @@
-package com.desafio.models;
+package com.desafio.model;
 
-import java.time.LocalDateTime;
+import java.time.LocalDate;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
 
 import lombok.Data;
 
 @Data
+@Entity
+@Table(name = "cliente")
 public class Cliente {
 	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	@Column(name = "nome_cliente", nullable = false)
 	private String nomeCliente;
+	@Column(name = "sexo", nullable = false)
 	private String sexo;
-	private LocalDateTime dataNascimento;
+	@Column(name = "data_nascimento", nullable = false)
+	private LocalDate dataNascimento;
+	@Column(name = "idade", nullable = false)
 	private Integer idade;
+	
+	@ManyToOne
 	private Cidade cidade;
 	
 }
