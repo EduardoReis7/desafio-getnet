94f170f72236cadf1e2c2dad66ff0ad3d766358b
EduardoReis7 <eduardodovigireis@gmail.com>
2020-05-07 14:53:06 -0300

    Refatoração da classe Response

--- src/main/java/com/desafio/response/Response.java
+++ src/main/java/com/desafio/response/Response.java
@@ -1,8 +1,5 @@
 package com.desafio.response;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import lombok.Getter;
 import lombok.Setter;
 import lombok.NoArgsConstructor;
@@ -13,12 +10,4 @@ import lombok.NoArgsConstructor;
 public class Response<T> {
 	
 	private T data;
-	private List<String> errors;
-	
-	public List<String> getErrors() {
-		if(this.errors == null) {
-			this.errors = new ArrayList<String>();
-		}
-		return errors;
-	}
 }
